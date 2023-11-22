87c72fadcce7955cbacd42beba1d3bb2243b28b8
Ajay <ajay0912.dpi@gmail.com>
2023-11-23 01:44:22 +0530

    search api + PLP page integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,42 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import proxyMiddleware from './plugins/proxy-middleware.mjs';
-
 import react from "@astrojs/react";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-
   adapter: node({
     mode: "standalone",
   }),
-
   integrations: [
-    proxyMiddleware("/payment_proxy", {
-      target: "https://api.razorpay.com",
+    // proxyMiddleware("/payment_proxy", {
+    //   target: "https://api.razorpay.com",
+    //   changeOrigin: true,
+    //   pathRewrite: {
+    //     "/payment_proxy": "",
+    //   },
+    // }),
+
+    proxyMiddleware("/petstore_proxy", {
+      target: "http://localhost:8082",
       changeOrigin: true,
-      pathRewrite: {'/payment_proxy': ''}
+      pathRewrite: {
+        "/petstore_proxy": "",
+      },
     }),
 
-	react(),
+    react(),
+    vue({
+      template: {
+        compilerOptions: {
+          // treat any tag that starts with ion- as custom elements
+          isCustomElement: (tag) => tag.startsWith("ion-"),
+        },
+      },
+      // ...
+    }),
   ],
 });
